1063937ec814602144dc808caf605b8b673db2a1
Максим <klimanovics_maksims@hotmail.com>
2018-11-27 11:10:47 +0200

    Formatting features

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -8,14 +8,12 @@ export default class Composer extends Component {
     render() {
         return (
             <section>
-                <section>
-                    <img src = { avatar } />
-                    <form>
-                        <textarea placeholder = {'What is on your mind, Lisa?'} />
-                        <input type = 'submit' value = 'Post' />
-                    </form>
-                </section> 
-            </section>
+                <img src = { avatar } />
+                <form>
+                    <textarea placeholder = {'What is on your mind, Lisa?'} />
+                    <input type = 'submit' value = 'Post' />
+                </form>
+            </section> 
         );
     }
 }
--- source/components/Post/Index.js
+++ source/components/Post/Index.js
@@ -3,17 +3,16 @@ import React, { Component } from 'react';
 
 //Instruments
 import avatar from 'theme/assets/lisa';
+import moment from 'moment';
 
 export default class Post extends Component {
     render() {
         return (
             <section>
-                <section>
-                    <img src = { avatar } />
-                    <a>Lisa Simpson</a>
-                    <time>Today</time>
-                    <p>How You Doooooin!</p>
-                </section>
+                <img src = { avatar } />
+                <a>Lisa Simpson</a>
+                <time>{moment().format('DD.MM.YYYY HH:mm:ss')}</time>
+                <p>How You Doooooin!</p>
             </section>
         );
     }
