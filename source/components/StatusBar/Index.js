21256049d7632c957f9e1af553e1dfece89aaa4e
Максим <klimanovics_maksims@hotmail.com>
2018-11-29 07:45:22 +0200

    Changed syntax

--- source/components/StatusBar/Index.js
+++ source/components/StatusBar/Index.js
@@ -7,8 +7,7 @@ import Styles from './styles.m.css';
 export default class StatusBar extends Component {
     render() {
         return (
-            <section className={ Styles.statusBar }>
-            </section>
+            <section className={ Styles.statusBar } />
         );
     }
 }
