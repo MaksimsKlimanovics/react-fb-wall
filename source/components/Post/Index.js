95929fbfee7c170dcf7224ddf74f97d06af61868
Максим <klimanovics_maksims@hotmail.com>
2018-11-29 12:42:10 +0200

    added Post style

--- source/components/Post/Index.js
+++ source/components/Post/Index.js
@@ -4,11 +4,12 @@ import React, { Component } from 'react';
 //Instruments
 import avatar from 'theme/assets/lisa';
 import moment from 'moment';
+import Styles from './styles.m.css';
 
 export default class Post extends Component {
     render() {
         return (
-            <section>
+            <section className = { Styles.post }>
                 <img src = { avatar } />
                 <a>Lisa Simpson</a>
                 <time>{moment().format('DD.MM.YYYY HH:mm:ss')}</time>
