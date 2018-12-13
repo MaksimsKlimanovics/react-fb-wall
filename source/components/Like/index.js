c2be396bc73253387724e9881df87f7acfefd318
Максим <klimanovics_maksims@hotmail.com>
2018-12-13 08:21:07 +0200

    added isRequired

--- source/components/Like/index.js
+++ source/components/Like/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import Styles from './styles.m.css';
+import { string, arrayOf, shape, func } from 'prop-types';
 
-export default class Like extends Component {
+export class Like extends Component {
+    static propTypes = {
+        _likePost: func.isRequired,
+        id: string.isRequired,
+        likes: arrayOf(
+            shape({
+                id: string.isRequired,
+                firstName: string.isRequired,
+                lastName: string.isRequired,
+            })
+        ).isRequired,
+    };
+
     render() {
+        console.log('this.props', this.props);
+
         return 'like';
     }
 }
