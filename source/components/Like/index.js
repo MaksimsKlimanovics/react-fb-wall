caeac92800eb757d1dd51f215a23cc69006c1b66
Максим <klimanovics_maksims@hotmail.com>
2018-12-13 11:15:23 +0200

    Extend Like component

--- source/components/Like/index.js
+++ source/components/Like/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Styles from './styles.m.css';
 import { string, arrayOf, shape, func } from 'prop-types';
+import cx from 'classNames';
 
 export class Like extends Component {
     static propTypes = {
@@ -11,13 +12,44 @@ export class Like extends Component {
                 id: string.isRequired,
                 firstName: string.isRequired,
                 lastName: string.isRequired,
-            })
+            }),
         ).isRequired,
     };
 
-    render() {
-        console.log('this.props', this.props);
+    constructor () {
+        super();
+        this._getLikedByMe = this._getLikedByMe.bind(this);
+        this._getLikeStyles = this._getLikeStyles.bind(this);
+    }
 
-        return 'like';
+    _getLikedByMe() {
+        const { currentUserFirstName, currentUserLastName, likes } = this.props;
+
+        return likes.some(({ firstName, lastName }) => {
+            return (
+                `${firstName} ${lastName}` === `${currentUserFirstName} ${currentUserLastName}`
+            );
+        });
+    }
+
+    _getLikeStyles() {
+        const likedByMe = this._getLikedByMe();
+
+        return cx(Styles.icon, {
+            [ Styles.liked ]: likedByMe,
+        });
+    }
+
+    render() {
+        //console.log('this.props', this.props);
+        const likeStyles = this._getLikeStyles();
+
+        return (
+            <section className = { Styles.like }>
+                <span className = { likeStyles } onClick = { this._likePost }>
+                    Like
+                </span>
+            </section>
+        );
     }
 }
